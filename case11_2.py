80e3e232f411b85986b3a627c52dd52e656554e6
Alexandr Selishchev <44421225+selishchev@users.noreply.github.com>
2019-03-12 10:01:47 +0300

    Update case11_2.py

--- case11_2.py
+++ case11_2.py
@@ -5,11 +5,11 @@ def tree(l, n):
     if n == 0:
         return
     turtle.forward(l)
-    turtle.right(45)
+    turtle.right(30)
     tree(0.75*l, n-1)
-    turtle.left(90)
+    turtle.left(60)
     tree(0.75*l, n-1)
-    turtle.right(45)
+    turtle.right(30)
     turtle.backward(l)
 
 
